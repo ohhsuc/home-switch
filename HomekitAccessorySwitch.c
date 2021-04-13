bf45b53987a823c6b746d7ba20e7601cf4904a8c
Rulee Chen <rulee@live.cn>
2021-04-13 19:18:31 +0800

    add dev tools

--- HomekitAccessorySwitch.c
+++ HomekitAccessorySwitch.c
@@ -13,13 +13,13 @@ void my_accessory_identify(homekit_value_t _value) {
 homekit_characteristic_t cha_switch_on = HOMEKIT_CHARACTERISTIC_(ON, false);
 
 // format: string; HAP section 9.62; max length 64
-homekit_characteristic_t cha_name = HOMEKIT_CHARACTERISTIC_(NAME, "Switch");
+homekit_characteristic_t cha_name = HOMEKIT_CHARACTERISTIC_(NAME, "PurlSwitch");
 
 homekit_accessory_t *accessories[] = {
   HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {
     HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
-      HOMEKIT_CHARACTERISTIC(NAME, "Switch"),
-      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "RuleeHome Inc."),
+      HOMEKIT_CHARACTERISTIC(NAME, "PurlSwitch"),
+      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Victoria 9# Inc."),
       HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
       HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
       HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
