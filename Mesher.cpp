8d8680a2cccec7a1f2f8d91bb9d79601885755a9
Rulee Chen <rulee@live.cn>
2021-05-29 14:43:56 +0800

    update mesher

--- Mesher.cpp
+++ Mesher.cpp
@@ -5,31 +5,75 @@ namespace Victoria {
   namespace Components {
 
     // IMeshLoader
-    IMeshLoader::IMeshLoader() {
+    IMeshLoader::IMeshLoader(uint8_t inputPin) {
+      _inputPin = inputPin;
     }
     void IMeshLoader::onMessage(TMessageHandler handler) {
+      _messageHandler = handler;
+    }
+    void IMeshLoader::send(MeshMessage& message) {
+      Serial.println("IMeshLoader send()");
     }
 
     // RadioFrequencyMeshLoader
-    RadioFrequencyMeshLoader::RadioFrequencyMeshLoader(uint8_t inputPin) {
+    RadioFrequencyMeshLoader::RadioFrequencyMeshLoader(uint8_t inputPin) : IMeshLoader(inputPin) {
     }
-    void RadioFrequencyMeshLoader::send() {
+    void RadioFrequencyMeshLoader::send(MeshMessage& message) {
+      Serial.println("RadioFrequencyMeshLoader send()");
     }
 
     // BluetoothMeshLoader
-    BluetoothMeshLoader::BluetoothMeshLoader(uint8_t inputPin) {
+    BluetoothMeshLoader::BluetoothMeshLoader(uint8_t inputPin) : IMeshLoader(inputPin) {
     }
-    void BluetoothMeshLoader::send() {
+    void BluetoothMeshLoader::send(MeshMessage& message) {
+      Serial.println("BluetoothMeshLoader send()");
     }
 
     // Mesher
     Mesher::Mesher() {
-    }
-    void Mesher::_handleMessage(MeshMessage& message) {
+      _id = CommonHelpers::randomString(8);
     }
     void Mesher::setLoader(IMeshLoader* loader) {
       _loader = loader;
-      _loader->send();
+      _loader->onMessage(std::bind(&Mesher::_handleMessage, this, std::placeholders::_1));
+    }
+    void Mesher::_handleMessage(MeshMessage& message) {
+      if (message.type == MESH_HEARTBEAT) { // heartbeat
+        if (message.sourceId == _id) {
+          // ignore (launched by ourself)
+        } else {
+          message.replyId = _id;
+          message.content = "pong";
+          _loader->send(message);
+        }
+      } else if (message.type == MESH_WIFI_REQUEST) {
+        if (message.sourceId == _id) {
+          // check wifi
+          // connect wifi
+        } else {
+          // check wifi
+          // reply wifi credential
+        }
+      } else if (message.type == MESH_WIFI_CONNECTED) {
+        if (message.sourceId == _id) {
+          // ignore (launched by ourself)
+        } else {
+          // check wifi
+          // connect wifi
+        }
+      }
+    }
+    void Mesher::send(MeshMessageType type) {
+      send(type, "");
+    }
+    void Mesher::send(MeshMessageType type, String content) {
+      MeshMessage message = {
+        sourceId: _id,
+        replyId: "",
+        type: type,
+        content: content,
+      };
+      _loader->send(message);
     }
 
   }
--- Mesher.h
+++ Mesher.h
@@ -3,39 +3,55 @@
 
 #include <functional>
 #include "Arduino.h"
+#include "Commons.h"
 
 namespace Victoria {
   namespace Components {
+
+    enum MeshMessageType {
+      MESH_HEARTBEAT,
+      MESH_WIFI_REQUEST,
+      MESH_WIFI_CONNECTED,
+    };
+
     struct MeshMessage {
-      String channel;
+      String sourceId;
+      String replyId;
+      MeshMessageType type;
       String content;
     };
 
     class IMeshLoader {
-      typedef std::function<void()> TMessageHandler;
+      typedef std::function<void(MeshMessage&)> TMessageHandler;
       public:
-        IMeshLoader();
+        IMeshLoader(uint8_t inputPin);
         void onMessage(TMessageHandler handler);
-        virtual void send();
+        virtual void send(MeshMessage& message);
+      protected:
+        uint8_t _inputPin;
+        TMessageHandler _messageHandler;
     };
 
     class RadioFrequencyMeshLoader: public IMeshLoader {
       public:
         RadioFrequencyMeshLoader(uint8_t inputPin);
-        void send() override;
+        virtual void send(MeshMessage& message);
     };
 
     class BluetoothMeshLoader: public IMeshLoader {
       public:
         BluetoothMeshLoader(uint8_t inputPin);
-        void send() override;
+        virtual void send(MeshMessage& message);
     };
 
     class Mesher {
       public:
         Mesher();
         void setLoader(IMeshLoader* loader);
+        void send(MeshMessageType type);
+        void send(MeshMessageType type, String content);
       private:
+        String _id;
         IMeshLoader* _loader;
         void _handleMessage(MeshMessage& message);
     };
